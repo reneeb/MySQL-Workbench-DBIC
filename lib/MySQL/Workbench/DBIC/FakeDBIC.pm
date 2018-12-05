54730918ce59fb6622c615d86f7cff73eccdf8dd
reneeb <info@perl-services.de>
2018-12-05 10:13:10 +0100

    various changes

--- lib/MySQL/Workbench/DBIC/FakeDBIC.pm
+++ lib/MySQL/Workbench/DBIC/FakeDBIC.pm
@@ -1,7 +1,18 @@
 package MySQL::Workbench::DBIC::FakeDBIC;
 
+# ABSTRACT: provide some DBIx::Class method stubs when DBIx::Class is not installed 
+
 use strict;
 use warnings;
+
+=head1 DESCRIPTION
+
+C<MySQL::Workbench::DBIC> tries to load the schema class to determine what version number
+should be used. This fails when L<DBIx::Class> is not installed. So we fake it.
+
+=head1 METHODS OVERLOADED
+
+=cut
 
 package
   DBIx::Class::Schema;
@@ -9,9 +20,17 @@ package
 use strict;
 use warnings;
 
-# ABSTRACT: provide some DBIx::Class method stubs when DBIx::Class is not installed 
-
 sub DBIx::Class::Schema::load_namespaces {}
+
+=head2 DBIx::Class::Schema
+
+=over 4
+
+=item * load_namespaces
+
+=back
+
+=cut
 
 package
    DBIx::Class;
@@ -26,10 +45,24 @@ sub DBIx::Class::set_primary_key {}
 sub DBIx::Class::belongs_to {}
 sub DBIx::Class::has_many {}
 
+=head2 DBIx::Class
+
+=over 4
+
+=item * load_components
+
+=item * DBIx::Class::table
+
+=item * add_columns
+
+=item * set_primary_key
+
+=item * belongs_to
+
+=item * has_many
+
+=back
+
+=cut
+
 1;
-
-=head1 DESCRIPTION
-
-C<MySQL::Workbench::DBIC> tries to load the schema class to determine what version number
-should be used. This fails when L<DBIx::Class> is not installed. So we fake it.
-
