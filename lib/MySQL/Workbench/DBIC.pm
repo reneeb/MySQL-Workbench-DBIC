4f2c10feceaa1825e9574d645253836a29ed1f71
reneeb <info@perl-services.de>
2019-03-17 09:55:07 +0100

    prepare release

--- lib/MySQL/Workbench/DBIC.pm
+++ lib/MySQL/Workbench/DBIC.pm
@@ -14,7 +14,7 @@ use MySQL::Workbench::Parser;
 
 # ABSTRACT: create DBIC scheme for MySQL workbench .mwb files
 
-our $VERSION = '1.16';
+our $VERSION = '1.17';
 
 has output_path              => ( is => 'ro', required => 1, default => sub { '.' } );
 has file                     => ( is => 'ro', required => 1 );
